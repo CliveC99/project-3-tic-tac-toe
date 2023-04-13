3b995c563aa359521db8bfafd2be9d3bdf393937
CliveC99 <cliveconnell@gmail.com>
2023-04-13 08:38:52 +0000

    Add the board to be printed for the game

--- run.py
+++ run.py
@@ -14,3 +14,21 @@ def intro():
     print("Tie: The board is filled and 'X' or 'O' are not in a row.\n")
     print("Are you ready to play?\n")
 intro()   
+
+
+# Global Variables
+board = ["-", "-", "-",
+         "-", "-", "-",
+         "-", "-", "-"]
+
+
+
+def print_board():
+    """
+    Displays the board for the game.
+    """
+    print(board[0] + " | " + board[1] + " | " + board[2] + "")
+    print(board[3] + " | " + board[4] + " | " + board[5] + "")
+    print(board[6] + " | " + board[7] + " | " + board[8] + "")
+
+print_board()
