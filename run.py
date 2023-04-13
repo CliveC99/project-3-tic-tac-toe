010418776e27a02f2b38b040f81acb73a3f60b29
CliveC99 <cliveconnell@gmail.com>
2023-04-13 08:40:36 +0000

    Add layout to the printed board

--- run.py
+++ run.py
@@ -27,8 +27,8 @@ def print_board():
     """
     Displays the board for the game.
     """
-    print(board[0] + " | " + board[1] + " | " + board[2] + "")
-    print(board[3] + " | " + board[4] + " | " + board[5] + "")
-    print(board[6] + " | " + board[7] + " | " + board[8] + "")
+    print(board[0] + " | " + board[1] + " | " + board[2] + "     1 | 2 | 3")
+    print(board[3] + " | " + board[4] + " | " + board[5] + "     4 | 5 | 6")
+    print(board[6] + " | " + board[7] + " | " + board[8] + "     7 | 8 | 9")
 
 print_board()
