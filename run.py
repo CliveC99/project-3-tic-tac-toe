383d4b434ae2976dd2c133ca60f83763e9851226
CliveC99 <cliveconnell@gmail.com>
2023-04-13 08:35:56 +0000

    Add info section to appear

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+def intro():
+    """
+    Gives the user information and rules about the games.
+    Allows the user to start the game by entering 'y'.
+    Catches error for data input incorrectly.
+    """
+    print("Welcome to Tic-Tac-Toe, 2 player edition!")
+    print("\nRules:\n")
+    print("Player 1 is given 'X'")
+    print("Player 2 is given 'O'\n")
+    print("A 3x3 board is printed and is marked 1-9\n")
+    print("Win: If 3 spots are filled in a row, column or diagonally.\n")
+    print("Tie: The board is filled and 'X' or 'O' are not in a row.\n")
+    print("Are you ready to play?\n")
+intro()   
